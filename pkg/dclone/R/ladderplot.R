c1cd925fef21af87d3c8244f1746f2e56e270143
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-15 18:45:27 +0000

    factor ordering bug fixed

--- pkg/dclone/R/ladderplot.R
+++ pkg/dclone/R/ladderplot.R
@@ -1,6 +1,7 @@
 ladderplot <-
 function(x, col, pch=19, lty=1, vertical = TRUE, ...)
 {
+    x <- as.data.frame(x)
     if (NCOL(x) < 2)
         stop("'x' must have at least 2 columns")
     if (missing(col))
@@ -12,14 +13,15 @@ function(x, col, pch=19, lty=1, vertical = TRUE, ...)
         xlim <- c(0,ncol(x)+1)
         ylim <- range(x)
         with(y, stripchart(values ~ ind, pch=pch, ylim=ylim, xlim=xlim, vertical=vertical, ...))
-        lapply(1:ncol(x), function(i) points(cbind(rep(i,nrow(x)), x[,i]), col=col, pch=pch))
-        lapply(1:nrow(x), function(i) lines(cbind(1:ncol(x), as.matrix(x)[i,]), col=col[i], lty=lty))
+        id <- match(colnames(x),levels(y$ind))
+        lapply(1:ncol(x), function(i) points(cbind(rep(i,nrow(x)), x[,id[i]]), col=col, pch=pch))
+        lapply(1:nrow(x), function(i) lines(cbind(id, as.matrix(x)[i,]), col=col[i], lty=lty))
     } else {
         xlim <- range(x)
         ylim <- c(0,ncol(x)+1)
         with(y, stripchart(values ~ ind, pch=pch, ylim=ylim, xlim=xlim, vertical=vertical, ...))
-        lapply(1:ncol(x), function(i) points(cbind(x[,i], rep(i,nrow(x))), col=col, pch=pch))
-        lapply(1:nrow(x), function(i) lines(cbind(as.matrix(x)[i,], 1:ncol(x)), col=col[i], lty=lty))
+        lapply(1:ncol(x), function(i) points(cbind(x[,id[i]], rep(i,nrow(x))), col=col, pch=pch))
+        lapply(1:nrow(x), function(i) lines(cbind(as.matrix(x)[i,], id), col=col[i], lty=lty))
     }
     invisible(NULL)
 }
