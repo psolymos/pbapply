476fbb5a25cce43cc27cfb5ab07174baff72f2c6
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-22 19:25:52 +0000

    bug fixes for factor ordering etc

--- pkg/dclone/R/ladderplot.R
+++ pkg/dclone/R/ladderplot.R
@@ -1,5 +1,5 @@
 ladderplot <-
-function(x, col, pch=19, lty=1, vertical = TRUE, ...)
+function(x, col, pch=19, lty=1, xlim=c(0,ncol(x)+1), ylim=range(x), vertical = TRUE, ...)
 {
     x <- as.data.frame(x)
     if (NCOL(x) < 2)
@@ -10,8 +10,8 @@ function(x, col, pch=19, lty=1, vertical = TRUE, ...)
         col <- rep(col, nrow(x))[1:nrow(x)]
     y <- stack(x)
     if (vertical) {
-        xlim <- c(0,ncol(x)+1)
-        ylim <- range(x)
+#        xlim <- c(0,ncol(x)+1)
+#        ylim <- range(x)
         with(y, stripchart(values ~ ind, pch=pch, ylim=ylim, xlim=xlim, vertical=vertical, ...))
         id <- match(colnames(x),levels(y$ind))
         lapply(1:ncol(x), function(i) points(cbind(rep(i,nrow(x)), x[,id[i]]), col=col, pch=pch))
