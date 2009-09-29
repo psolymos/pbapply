19282f5ab2692275584416e1022b6e8ce114e14d
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-29 17:28:27 +0000

    bugfix

--- pkg/dclone/R/ladderplot.R
+++ pkg/dclone/R/ladderplot.R
@@ -9,11 +9,11 @@ function(x, col, pch=19, lty=1, xlim=c(0,ncol(x)+1), ylim=range(x), vertical = T
     if (length(col) < nrow(x))
         col <- rep(col, nrow(x))[1:nrow(x)]
     y <- stack(x)
+    id <- match(colnames(x),levels(y$ind))
     if (vertical) {
 #        xlim <- c(0,ncol(x)+1)
 #        ylim <- range(x)
         with(y, stripchart(values ~ ind, pch=pch, ylim=ylim, xlim=xlim, vertical=vertical, ...))
-        id <- match(colnames(x),levels(y$ind))
         lapply(1:ncol(x), function(i) points(cbind(rep(i,nrow(x)), x[,id[i]]), col=col, pch=pch))
         lapply(1:nrow(x), function(i) lines(cbind(id, as.matrix(x)[i,]), col=col[i], lty=lty))
     } else {
