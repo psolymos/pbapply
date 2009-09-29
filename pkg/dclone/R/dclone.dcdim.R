be041e3a8b101df02a0158124066a3b885b9062d
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-29 20:38:45 +0000

    bugfix

--- pkg/dclone/R/dclone.dcdim.R
+++ pkg/dclone/R/dclone.dcdim.R
@@ -4,9 +4,13 @@ function(x, n.clones=1, attrib=TRUE, ...)
     if (n.clones==1)
         return(x)
     DIM <- dim(x)
+    clch <- paste("clone", 1:n.clones, sep=".")
     if (is.null(DIM)) {
         DIM <- length(x)
-        DIMNAM <- list(names(x), paste("clone", 1:n.clones, sep="."))
+        DIMNAM <- list(names(x), clch)
+    } else {
+        DIMNAM <- dimnames(x)
+        DIMNAM[[(length(DIMNAM) + 1)]] <- clch
     }
     rval <- array(rep(x, n.clones), dim=c(DIM, n.clones), dimnames=DIMNAM)
     if (attrib) {
