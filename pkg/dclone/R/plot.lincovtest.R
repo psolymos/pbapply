5632a407c556a975bba8dd8b06af0125be32c5cd
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-14 17:09:19 +0000

    dependencies

--- pkg/dclone/R/plot.lincovtest.R
+++
@@ -1,9 +0,0 @@
-plot.lincovtest <-
-function(x, type = "b", ylab = "Estimated coefficients", xlab = "Quantiles", ...)
-{
-    plot(as.numeric(x), type = type, axes = FALSE, ylab = ylab, xlab = xlab, ...)
-    axis(2)
-    axis(1, at = 1:length(x), labels = names(x))
-    abline(x[1], 0, lty = 2)
-    invisible(x)
-}
