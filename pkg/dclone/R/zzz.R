a804ea9508c97f3e4dfb0f639bdfd9ea8f050d28
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-14 17:33:22 +0000

    dclone.crit options are back

--- pkg/dclone/R/zzz.R
+++ pkg/dclone/R/zzz.R
@@ -10,10 +10,14 @@
     ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), 
                     fields=c("Version", "Date"))
     cat(paste(pkgname, ver[1], "\t", ver[2], "\n"))
+    if (is.null(getOption("dclone.crit"))) {
+        options("dclone.crit"=c(lmax=0.05, pshw=0.05, rhat=1.1))
     invisible(NULL)
 }
 
 .onUnload <- function(){
+    if (!is.null(getOption("dclone.crit"))) {
+        options("dclone.crit"=NULL)
     invisible(NULL)
 }
 
