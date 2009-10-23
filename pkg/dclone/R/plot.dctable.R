d2e52157e78e98df764f540c239573a0d1291323
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-23 20:29:52 +0000

    fixes

--- pkg/dclone/R/plot.dctable.R
+++ pkg/dclone/R/plot.dctable.R
@@ -1,32 +1,36 @@
 plot.dctable <-
-function(x, which = 1:length(x), position = "topright", box.cex = 1, ...)
+function(x, which = 1:length(x), position = "topright", box.cex = 0.75, ...)
 {
     plot1 <- function(param, show.legend, ...) {
         y <- x[[param]]
-        xlim <- range(y$n.clones - w/2, y$n.clones + w/2)
+        xlim <- range(xval - w/2, xval + w/2)
         ylim <- range(y[,"2.5%"], y[,"97.5%"], y$mean - y$sd, y$mean + y$sd)
         pch <- rep(21, nrow(y))
         if (!is.null(y$r.hat)) {
             pch[y$r.hat < crit] <- 19
         }
-        plot(y$n.clones, y$mean, ylim=ylim, xlim=xlim, pch=pch, type = "b", lty=2,
+        plot(xval, y$mean, ylim=ylim, xlim=xlim, pch=pch, type = "b", lty=2,
             xlab = "Number of clones", ylab="Estimate",
-            main = param, ...)
+            main = param, axes = FALSE, ...)
+        axis(1, xval, k)
+        axis(2)
+        box()
         if (!is.null(y$r.hat) && show.legend) {
             legend(position, pch=c(21, 19),
                 legend=c(paste("R.hat >= ", round(crit, 1), sep=""),
                 paste("R.hat < ", round(crit, 1), sep="")))
         }
-        errlines(y$n.clones, cbind(y$mean - y$sd, y$mean + y$sd))
-        errlines(y$n.clones, cbind(y[,"25%"], y[,"50%"]), width=w, code=3, type="b")
-        errlines(y$n.clones, cbind(y[,"50%"], y[,"75%"]), width=w, code=3, type="b")
-        points(y$n.clones, y[,"2.5%"], pch="x")
-        points(y$n.clones, y[,"97.5%"], pch="x")
+        errlines(xval, cbind(y$mean - y$sd, y$mean + y$sd))
+        errlines(xval, cbind(y[,"25%"], y[,"50%"]), width=w, code=3, type="b")
+        errlines(xval, cbind(y[,"50%"], y[,"75%"]), width=w, code=3, type="b")
+        points(xval, y[,"2.5%"], pch="x")
+        points(xval, y[,"97.5%"], pch="x")
     }
+    k <- x[[1]]$n.clones
+    xval <- 1:length(k)
     crit <- getOption("dclone.crit")["rhat"]
     nam <- names(x)[which]
-    w <- if (length(x[[1]]$n.clones) > 1)
-        min(diff(x[[1]]$n.clones)) * 0.25 * box.cex else box.cex
+    w <- box.cex
     m <- length(which)
     if (m <= 3) {
         nr <- 1
