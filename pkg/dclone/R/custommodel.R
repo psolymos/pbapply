2f0ce0a1ffb1e92c01f3ff4db6872f1c5e8c8a6b
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-26 05:49:13 +0000

    custommodel can take character arg

--- pkg/dclone/R/custommodel.R
+++ pkg/dclone/R/custommodel.R
@@ -1,10 +1,14 @@
 custommodel <-
 function (model, exclude=NULL)
 {
-    model.text <- attr(model, "source")
-    model.text <- sub("^\\s*function\\s*\\(\\s*\\)", "model", 
-        model.text)
-    model.text <- gsub("%_%", "", model.text)
+    if (is.function(model)) {
+        model.text <- attr(model, "source")
+        model.text <- sub("^\\s*function\\s*\\(\\s*\\)", "model", 
+            model.text)
+        model.text <- gsub("%_%", "", model.text)
+    } else {
+        model.text <- as.character(unlist(model))
+    }
     incl <- 1:length(model.text)
     if (!is.null(exclude))
         incl <- incl[!(incl %in% exclude)]
