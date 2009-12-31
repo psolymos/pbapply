4746270946e58d6ab361eb2c8bdf247f3ca59d3f
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-31 16:41:05 +0000

    cleanup

--- pkg/dclone/R/summary.mcmc.list.dc.R
+++ pkg/dclone/R/summary.mcmc.list.dc.R
@@ -3,10 +3,10 @@ function(object, quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975), ...)
 {
     class(object) <- "mcmc.list"
     out <- summary(object, quantiles = quantiles, ...)
-    k <- nclones(object)
-    out$nclones <- k
+#    k <- nclones(object)
+    out$nclones <- nclones(object)
     nch <- nchain(object)
-    n <- niter(object)
+#    n <- niter(object)
     out$statistics <- cbind(out$statistics[,1:2],
         "DC SD"=dcsd(object), out$statistics[,3:4])
     if (nch > 1) {
