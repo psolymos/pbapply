fa0e1e93ca7ddd4dad14c0d04e6563c5c3d73636
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-15 06:51:26 +0000

    dcpar split

--- pkg/dclone/R/zzz.R
+++ pkg/dclone/R/zzz.R
@@ -5,15 +5,11 @@
     if (is.null(getOption("dclone.crit"))) {
         options("dclone.crit"=list(r.hat=1.1))
     }
-    if (is.null(getOption("dclone.cluster"))) {
-        options("dclone.cluster"=list(load.balancing=TRUE))
-    }
     invisible(NULL)
 }
 
 .onUnload <- function(libpath){
     options("dclone.crit"=NULL)
-    options("dclone.cluster"=NULL)
     invisible(NULL)
 }
 
