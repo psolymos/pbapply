6532a7409c785dc300f20b26c1511850e0362f3c
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-18 21:08:47 +0000

    probs arg

--- pkg/dclone/R/dcdiag.default.R
+++ pkg/dclone/R/dcdiag.default.R
@@ -1,5 +1,5 @@
 dcdiag.default <-
-function(x, ...) {
+function(x, probs = c(0, 1), ...) {
     dct <- is.null(attr(x, "dcdiag"))
     ll <- length(list(...))
     if (ll || (!ll && dct)) {
@@ -10,7 +10,7 @@ function(x, ...) {
         ord <- order(k)
         obj <- obj[ord]
         k <- k[ord]
-        dctmp <- t(sapply(obj, extractdcdiag.default))
+        dctmp <- t(sapply(obj, extractdcdiag.default, probs=probs))
         Call <- match.call()
         mnam <- as.character(Call[-1])
         rval <- as.data.frame(dctmp)
