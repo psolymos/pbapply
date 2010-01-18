127586805f090c5244501f935ccd3fa06e57c78f
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-18 07:40:32 +0000

    plot

--- pkg/dclone/R/plot.chisq.diag.R
+++ pkg/dclone/R/plot.chisq.diag.R
@@ -1,9 +1,11 @@
 plot.chisq.diag <- function(x, 
     xlab = "Theoretical Quantiles",
-    ylab = "Empirical Quantiles", ...)
+    ylab = "Empirical Quantiles", line = TRUE, ...)
 {
     qq <- x$quantiles
     qqplot(qq$theoretical, qq$empirical, 
         plot.it = TRUE, xlab = xlab, ylab = ylab, ...)
+    if (line)
+        abline(0, 1, lty = 2)
     invisible(x)
 }
