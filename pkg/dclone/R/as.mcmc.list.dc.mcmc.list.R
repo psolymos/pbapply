65a76a70ee94da124511e6661ef438ecd568c779
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-20 17:00:50 +0000

    nclones arg can be set

--- pkg/dclone/R/as.mcmc.list.dc.mcmc.list.R
+++ pkg/dclone/R/as.mcmc.list.dc.mcmc.list.R
@@ -1,8 +1,8 @@
 as.mcmc.list.dc.mcmc.list <-
-function(x, ...)
+function(x, n.clones=nclones(x), ...)
 {
     class(x) <- c("mcmc.list.dc", class(x))
-    attr(x, "n.clones") <- nclones(x)
+    attr(x, "n.clones") <- n.clones
     x
 }
 
