43bcd4a9b8448fb95ac79be36a73be2df792ae68
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-27 23:07:06 +0000

    lb issues

--- pkg/dcpar/R/snowWrapper.R
+++ pkg/dcpar/R/snowWrapper.R
@@ -1,7 +1,9 @@
 snowWrapper <-
-function(cl, seq, fun, cldata, name="cldata", lib=NULL, size = 1, load.balancing = FALSE, 
-seed=1, kind="default", normal.kind="default", ...)
+function(cl, seq, fun, cldata, name="cldata", lib=NULL, size = 1, 
+seed=1, kind="default", normal.kind="default", 
+balancing=c("none", "load", "size"), ...)
 {
+    balancing <- match.arg(balancing)
     clusterSeed(cl, seed, kind, normal.kind)
     ## if object name exists in global env, make a copy as tmp, and put back in the end
     if (exists(name, envir=.GlobalEnv)) {
@@ -10,13 +12,17 @@ seed=1, kind="default", normal.kind="default", ...)
         on.exit(assign(name, tmp, envir = .GlobalEnv), add=TRUE)
     }
     ## loads lib on each worker
-    if (!is.null(lib))
+    if (!is.null(lib)) {
         for (i in lib)
             eval(parse(text=paste("clusterEvalQ(cl, library(", i, "))")))
+    }
     ## place object name into global env (clusterExport can reach it)
     assign(name, cldata, envir = .GlobalEnv)
     clusterExport(cl, name)
     ## parallel work done here similarly as in parLapply
-    res <- function(cl, seq, fun, size=size, load.balancing=load.balancing, ...)
+    res <- switch(balancing,
+        "none" = clusterApplyLB(cl, seq, fun, ...),
+        "load" = parLapply(cl, seq, fun, ...),
+        "size" = parLapplySB(cl, seq, size=size, fun, ...))
     res
 }
