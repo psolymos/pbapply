f0bb844cca2e10d73798ea37c3e4d0beacfa3ae7
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-12 06:10:07 +0000

    options

--- pkg/dclone/R/zzz.R
+++ pkg/dclone/R/zzz.R
@@ -3,21 +3,19 @@
                     fields=c("Version", "Date"))
     cat(paste(pkgname, ver[1], "\t", ver[2], "\n"))
     ## dclone options setup
-    if (is.null(getOption("dclone"))) {
+    if (is.null(getOption("dclone.rhat")))
         options("dclone.rhat"=1.1)
+    if (is.null(getOption("dclone.autoburnin")))
         options("dclone.autoburnin"=TRUE)
+    if (is.null(getOption("dclone.diag")))
         options("dclone.diag"=0.05)
+    if (is.null(getOption("dclone.verbose")))
         options("dclone.verbose"=1)
-#        dcoptions <- list(r.hat = list(crit = 1.1, autoburnin = TRUE),
-#            dcdiag = list(crit = 0.05),
-#            verbose = 1)
-#        options("dclone"=dcoptions)
-    }
     invisible(NULL)
 }
 
 .onUnload <- function(libpath){
-#    options("dclone"=NULL)
+    ## remove dclone options
     options("dclone.rhat"=NULL)
     options("dclone.autoburnin"=NULL)
     options("dclone.diag"=NULL)
--- pkg/dcpar/R/zzz.R
+++ pkg/dcpar/R/zzz.R
@@ -2,9 +2,8 @@
     ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), 
                     fields=c("Version", "Date"))
     cat(paste(pkgname, ver[1], "\t", ver[2], "\n"))
-    if (is.null(getOption("dcpar"))) {
+    if (is.null(getOption("dcpar.LB")))
         options("dcpar.LB"=FALSE)
-    }
     invisible(NULL)
 }
 
