da82ece5a05c9d767f0b558a05e48a384c256079
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-07 19:41:54 +0000

    one par fix

--- pkg/dclone/R/summary.mcmc.list.dc.R
+++ pkg/dclone/R/summary.mcmc.list.dc.R
@@ -4,14 +4,17 @@ function(object, quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975), ...)
     class(object) <- "mcmc.list"
     out <- summary(object, quantiles = quantiles, ...)
     out$nclones <- nclones(object)
-    nch <- nchain(object)
-    out$statistics <- cbind(out$statistics[,1:2],
-        "DC SD"=dcsd(object), out$statistics[,3:4])
-    if (nch > 1) {
+    if (nchain(object) > 1) {
         abin <- getOption("dclone.autoburnin")
         rhat <- gelman.diag(object, autoburnin = abin)$psrf[,1]
     } else rhat <- NA
-    out$statistics <- cbind(out$statistics, "R hat" = rhat)
+    if (nvar(object) > 1) {
+        out$statistics <- cbind(out$statistics[,1:2], "DC SD"=dcsd(object), out$statistics[,3:4], "R hat" = rhat)
+    } else {
+        tmp <- c(out$statistics[1:2], "DC SD"=dcsd(object), out$statistics[3:4], "R hat" = rhat)
+        out$statistics <- matrix(tmp, nrow=1)
+        dimnames(out$statistics) <- list(varnames(object), names(tmp))
+    }
     class(out) <- "summary.mcmc.dc"
     out
 }
