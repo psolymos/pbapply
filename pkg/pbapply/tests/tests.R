8845670a8bc8dcd446c71d0fc12ebdbe247d6809
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-04 14:02:06 +0000

    new test

--- pkg/pbapply/tests/tests.R
+++ pkg/pbapply/tests/tests.R
@@ -3,3 +3,27 @@ example(pboptions)
 example(pbapply)
 example(lapply)
 example(apply)
+
+##
+#library(plyr)
+lapply_pb <- function(X, FUN, ...)
+{
+ env <- environment()
+ pb_Total <- length(X)
+ counter <- 0
+ pb <- txtProgressBar(min = 0, max = pb_Total, style = 3)   
+
+ # wrapper around FUN
+ wrapper <- function(...){
+   curVal <- get("counter", envir = env)
+   assign("counter", curVal +1 ,envir=env)
+   setTxtProgressBar(get("pb", envir=env), curVal +1)
+   FUN(...)
+ }
+ lapply(X, wrapper, ...)
+ close(pb)
+}
+system.time(x1 <- lapply(1:10, function(i) Sys.sleep(0.2)))
+system.time(x1 <- lapply_pb(1:10, function(i) Sys.sleep(0.2)))
+#system.time(x1 <- l_ply(1:10, function(i) Sys.sleep(0.2), .progress=create_progress_bar(name = "text")))
+system.time(x1 <- pblapply(1:10, function(i) Sys.sleep(0.2)))
