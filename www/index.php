40fdb03a7059446bdca6d2a33b308541daab825b
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-10 02:23:41 +0000

    sharx added

--- www/index.php
+++ www/index.php
@@ -47,25 +47,39 @@ echo $contents; } ?>
 
 <h2>Infrastructure for dclone</h2>
 
-<p>Because dclone is an R package, installing R is absolutely necessary. The choice of the preferred BUGS program is up to the
-user, but in order to load the dclone package, JAGS has to be installed so that rjags can find it.</p>
+<p>Because dclone is an R package, installing R is absolutely necessary. 
+The choice of the preferred BUGS program is up to the
+user, but in order to load the dclone package, JAGS has to be installed 
+so that rjags can find it.</p>
 
 <ul>
-<li><strong>R (>= 2.7.0):</strong> download from <a href="http://cran.r-project.org/">CRAN</a>.</li>
-<li><strong>WinBUGS:</strong> download <a href="http://www.mrc-bsu.cam.ac.uk/bugs/winbugs/contents.shtml">from here</a>, don't forget
-the patch and the immortality key, the <strong>R2WinBUGS</strong> and <strong>coda</strong> R packages are required for using WinBUGS from within R.</li>
-<li><strong>OpenBUGS:</strong> download <a href="http://www.openbugs.info/w/Downloads">from here</a>, it requires the
-<strong>BRugs (>= 0.3-2)</strong> R package that is available from <a href="http://www.stats.ox.ac.uk/pub/RWin/">CRAN Extras</a>.</li>
-<li><strong>JAGS:</strong> download <a href="http://sourceforge.net/projects/mcmc-jags/">from here</a>,
-the <strong>rjags</strong> and <strong>coda</strong> R packages are required for using JAGS from within R.</li>
+<li><strong>R (>= 2.7.0):</strong> download from 
+<a href="http://cran.r-project.org/">CRAN</a>.</li>
+<li><strong>WinBUGS:</strong> download 
+<a href="http://www.mrc-bsu.cam.ac.uk/bugs/winbugs/contents.shtml">from here</a>, don't forget
+the patch and the immortality key, the <strong>R2WinBUGS</strong> and 
+<strong>coda</strong> R packages are required for using WinBUGS from within R.</li>
+<li><strong>OpenBUGS:</strong> download 
+<a href="http://www.openbugs.info/w/Downloads">from here</a>, it requires the
+<strong>BRugs (>= 0.3-2)</strong> R package that is 
+available from <a href="http://www.stats.ox.ac.uk/pub/RWin/">CRAN Extras</a>.</li>
+<li><strong>JAGS:</strong> download 
+<a href="http://sourceforge.net/projects/mcmc-jags/">from here</a>,
+the <strong>rjags</strong> and <strong>coda</strong> 
+R packages are required for using JAGS from within R.</li>
 </ul>
 
 <p>The dclone package itself depends on the <strong>rjags</strong>, <strong>coda</strong> (>= 0.13), <strong>R2WinBUGS</strong>, <strong>snow</strong> and <strong>rlecuyer</strong> R packages (the latter two are for parallel computations) which are all available from CRAN. 
 The dclone package also suggests to have the <strong>BRugs</strong>, <strong>rsprng</strong> R packages but these might not be available for all
 operating systems. 
-The easiest way to install all required packages at once is to type this after opening R:</p>
+The easiest way to install the stable release and all required packages 
+at once is to type this after opening R:</p>
 
-<p><TT>install.packages("dclone", dependencies = TRUE)</TT></p>
+<p><TT>install.packages("dclone")</TT></p>
+
+<p>The development version can be installed as:</p>
+
+<p><TT>install.packages("dclone", repos = "http://r-forge.r-project.org")</TT></p>
 
 <h2>Additional resources</h2>
 
@@ -78,8 +92,11 @@ The easiest way to install all required packages at once is to type this after o
 <h2>Specialized packages in this repository</h2>
 
 <ul>
-<li><strong>pbapply:</strong> a lightweight package that adds progress bar to vectorized R functions ('*apply'). The implenentation can easily be added 
-  to functions, where showing the progress is useful for the user (e.g. bootstrap).</li>
+<li><strong>pbapply:</strong> a lightweight package that adds 
+progress bar to vectorized R functions ('*apply'). The implenentation can easily be added 
+to functions, where showing the progress is useful for the user (e.g. bootstrap).</li>
+<li><strong>sharx:</strong> data sets and SAR, SARX, HSAR and HSARX 
+  models as described in Solymos and Lele (in press).</li>
 </ul>
 
 </body>
