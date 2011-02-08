abe3fb6943422805d497c2172472f4d9478cf321
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-08 23:50:20 +0000

    tweaks

--- pkg/dctests/tests/dclone.R
+++ pkg/dctests/tests/dclone.R
@@ -2,8 +2,8 @@
 ## dontrun pieces indicated
 library(dclone)
 example(bugs.fit) # dontrun
-example(bugs.parfit) # dontrun
-example(clusterSeed)
+#example(bugs.parfit) # dontrun
+#example(clusterSeed)
 example(clusterSize)
 example(clusterSplitSB) # dontrun
 example(coef.mcmc.list) # dontrun
@@ -25,14 +25,3 @@ example(snowWrapper) # dontrun
 example(update.mcmc.list) # dontrun
 example(write.jags.model)
 
-example(bugs.fit) # dontrun
-example(bugs.parfit) # dontrun
-example(clusterSplitSB) # dontrun
-example(coef.mcmc.list) # dontrun
-example(dc.fit) # dontrun
-example(dc.parfit) # dontrun
-example(dctable) # dontrun
-example(jags.fit) # dontrun
-example(jags.parfit) # dontrun
-example(snowWrapper) # dontrun
-example(update.mcmc.list) # dontrun
