42460c828ac056df74a21cf2cccedb2a6442d79c
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-15 15:41:55 +0000

    mcmcapply simplified

--- pkg/dclone/R/mcmcapply.R
+++ pkg/dclone/R/mcmcapply.R
@@ -3,25 +3,8 @@ function(x, FUN, ...)
 {
     if (!inherits(x, "mcmc.list"))
         stop("object class is not 'mcmc.list'")
-    FUN <- match.fun(FUN)
-    nc <- nchain(x)
-    nv <- nvar(x)
-    ni <- nrow(x[[1]])
-    if (nv == 1) {
-        y <- matrix(unlist(x), ncol=1)
-    } else {
-        if (nc > 1) {
-            y <- vector("list", nv)
-            for (j in 1:nv) {
-                y[[j]] <- matrix(0, ni, nc)
-                for (i in 1:nc) {
-                    y[[j]][, i] <- x[[i]][, j]
-                }
-            }
-            y <- sapply(y, array)
-        } else y <- x[[1]]
-        colnames(y) <- varnames(x)
-    }
-    apply(y, 2, FUN, ...)
+    y <- as.matrix(x)
+    if (missing(FUN))
+        y else apply(y, 2, match.fun(FUN), ...)
 }
 
