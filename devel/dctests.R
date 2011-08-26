697254040eca7ee7e166cec4f65f4f66c0aafa13
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-26 01:23:07 +0000

    NEWS started

--- devel/dctests.R
+++ devel/dctests.R
@@ -17,18 +17,20 @@ for (topic in ff) {
     exampleDontRun(topic)
     cat("\n## END   <<<<<<<<<<<<<<    ", topic, "    >>>>>>>>>>>>>>>>>\n\n")
 }
-cat("\n\n## START <<<<<<<<<<<<<<    endmatter    >>>>>>>>>>>>>>>>>\n")
+#cat("\n\n## START <<<<<<<<<<<<<<    endmatter    >>>>>>>>>>>>>>>>>\n")
 x <- readLines("c:/svn/dcr/devel/tests/dctests.log")
 err <- c(grep("rror", x), grep("arning", x))
 fal <- grep("d error", x)
 err <- err[!(err %in% fal)]
 if (length(err)) {
     beg <- c(grep("## START <<<<<<<<<<<<<<", x), length(x))[-1]
+    fin <- grep("## END   <<<<<<<<<<<<<<", x)
     top <- gsub("     >>>>>>>>>>>>>>>>>", "", gsub("## START <<<<<<<<<<<<<<     ", "", x[beg]))
     y <- character(length(x))
     y[1:(beg[1]-1)] <- "begin"
     for (i in 1:(length(beg)-1))
         y[beg[i]:(beg[i+1]-1)] <- top[i]
+    y[(fin[length(fin)]:length(y))] <- "endmatter"
     y <- y[err]
     cat("\n\n##       <<<<<<<<<<<<<<    Errors/Warnings found    >>>>>>>>>>>>>>>>>\n\n")
     data.frame(Line=err, Topic=y, Text=x[err])
