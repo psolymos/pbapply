8784c2d991e75d9493d4f8f73c82f0b4ecbbd8be
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-31 15:15:08 +0000

    better parsing/printing for custommodel

--- pkg/dclone/R/print.custommodel.R
+++ pkg/dclone/R/print.custommodel.R
@@ -1,7 +1,16 @@
-print.custommodel <-
-function (x, deparse = FALSE, ...)
+print.custommodel <- 
+function (x, deparse = FALSE, ...) 
 {
-    if (deparse)
-        dput(x, control=NULL) else print.default(x, ...)
+    cat("Object of class \"custommodel\":\n\n")
+    if (deparse) {
+        cat("structure(\nc(\"", x[1], "\",\n")
+        for(i in x[2:(length(x)-1)])
+            cat("\"", i, "\",\n")
+        cat("\"", x[length(x)], "\"),\n")
+        cat("class = \"custommodel\")\n")
+    } else {
+        for(i in x)
+            cat(i, "\n")
+    }
     invisible(x)
 }
