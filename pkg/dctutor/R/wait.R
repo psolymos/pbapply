8c5e81b1aad76863eb5d82b47c6d8bb83efdea88
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-02 19:35:52 +0000

    dctutor is premature, see tutorials in www folder

--- pkg/dctutor/R/wait.R
+++
@@ -1,7 +0,0 @@
-wait <-
-function(ask = "Press any key to continue ... ", do = NULL)
-{
-    ANSWER <- readline(ask)
-    if (!is.null(do))
-        eval(do)
-}
--- pkg/dctutor/R/zzz.R
+++
@@ -1,5 +0,0 @@
-.First.lib <- function(lib, pkg){
-    cat("\nThis is the 'dctutor' package\n", 
-        "Type 'dctutor()' to list available tutorials\n\n", 
-        sep="")
-}
