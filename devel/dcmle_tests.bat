6d5361d3516d5ad4def61c04c6ddcc611a7d879f
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-28 16:11:51 +0000

    pre-release

---
+++ devel/dcmle_tests.bat
@@ -0,0 +1,28 @@
+@echo off
+echo.
+echo dcmle tests
+echo.
+cd \svn\dcr\devel\tests
+echo.
+echo build source package
+echo.
+R CMD build "c:/svn/dcr/pkg/dcmle"
+echo.
+echo build binary package
+echo.
+R CMD build "c:/svn/dcr/pkg/dcmle" --binary
+echo.
+echo install source package
+echo.
+R CMD INSTALL dcmle_0.1-0.tar.gz
+echo.
+echo check package
+echo.
+R CMD check "c:/svn/dcr/pkg/dcmle" --outdir="c:/svn/dcr/devel/tests/"
+echo.
+echo run dontrun examples in dcmle
+echo.
+::R CMD BATCH --vanilla "c:/svn/dcr/devel/dcmle_tests.R" "c:/svn/dcr/devel/tests/dcmle_tests.log"
+echo.
+echo done
+::exit
