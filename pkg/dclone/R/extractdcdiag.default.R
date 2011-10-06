30eac60e7a5c6ba63813121374a03350eeccbf53
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-06 20:35:05 +0000

    diag fix

--- pkg/dclone/R/extractdcdiag.default.R
+++ pkg/dclone/R/extractdcdiag.default.R
@@ -1,7 +1,17 @@
 extractdcdiag.default <- 
 function(x, ...)
 {
-    chisq <- chisq.diag(x)$statistics
+    lmax <- try(lambdamax.diag(x))
+    if (inherits(lmax, "try-error"))
+            lmax <- NA
+    chisq <- try(chisq.diag(x))
+    if (inherits(chisq, "try-error")) {
+        ms.error <- NA
+        r.squared <- NA
+    } else {
+        ms.error <- chisq$statistics$ms.error
+        r.squared <- chisq$statistics$r.squared
+    }
     n.clones <- ifelse(is.null(nclones(x)), 1, nclones(x))
     if (nchain(x) > 1) {
         abin <- getOption("dcoptions")$autoburnin
@@ -15,9 +25,9 @@ function(x, ...)
     } else rhat <- NA
     rval <- c(n.clones = n.clones,
 #        n.params = nvar(x),
-        lambda.max = lambdamax.diag(x),
-        ms.error = chisq$ms.error,
-        r.squared = chisq$r.squared,
+        lambda.max = lmax,
+        ms.error = ms.error,
+        r.squared = r.squared,
         r.hat = rhat)
     rval
 }
