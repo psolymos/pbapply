3bfbc0f5ebb7667f1be10a43472515df572e094e
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-06 05:28:11 +0000

    new funs

---
+++ pkg/dcmle/R/dcexample.R
@@ -0,0 +1,39 @@
+listDcExamples <- 
+function() 
+{
+    read.table("http://dcr.r-forge.r-project.org/examples/")
+}
+
+sourceDcExample <- 
+function(topic, assign.global=TRUE) 
+{
+    dcel <- listDcExamples()
+    TOPIC <- dcel[,"topic"]
+    if (missing(topic))
+        return(TOPIC)
+    e <- new.env()
+    eval(parse(dcel[TOPIC==topic,"href"]), e)
+    if (assign.global)
+        assign(topic, e[[topic]], envir=.GlobalEnv)
+    invisible(e[[topic]])
+}
+
+dcExample <- 
+function(topic, n.clones=NULL, ...) 
+{
+    x <- sourceDcExample(topic, assign.global=FALSE)
+    rval <- if (!is.null(n.clones))
+        dcmle(x, n.clones=n.clones, ...) else NULL
+    spacer <- paste(rep("=", nchar(topic) + 32), collapse="")
+    cat("\n", spacer, "\n    Data Cloning Example: \"", 
+        topic, "\"\n", spacer, "\n", sep="")
+    cat("\n\n< Structure of the Data/Model Template >\n\n")
+    str(x)
+    cat("\n")
+    if (!is.null(rval)) {
+        cat("\n< Summary of the Fitted MLE Object >\n\n")
+        heading <- paste("Summary of the \"", topic, "\" Example", sep="")
+        getMethod("summary","dcMle")(rval, heading)
+    }
+    invisible(rval)
+}
