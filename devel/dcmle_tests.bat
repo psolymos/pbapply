c20b13719de051c1c32368054b996fb0abcb3dd4
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-08 05:41:35 +0000

    version up

--- devel/dcmle_tests.bat
+++ devel/dcmle_tests.bat
@@ -14,7 +14,7 @@ R CMD build "c:/svn/dcr/pkg/dcmle" --binary
 echo.
 echo install source package
 echo.
-R CMD INSTALL dcmle_0.1-0.tar.gz
+R CMD INSTALL dcmle_0.1-1.tar.gz
 echo.
 echo check package
 echo.
