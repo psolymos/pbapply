317a56259e8be5165644ceaf507f76077d2980c1
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-19 19:10:37 +0000

    mc features added

--- pkg/dclone/R/snowWrapper.R
+++ pkg/dclone/R/snowWrapper.R
@@ -1,3 +1,4 @@
+## set cl = getOption("mc.cores") as default ???
 snowWrapper <-
 function(cl, seq, fun, cldata, name = "cldata", use.env = FALSE,
 lib = NULL, dir = NULL, evalq = NULL,
@@ -5,7 +6,8 @@ size = 1, balancing = c("none", "load", "size", "both"),
 rng.type = c("none", "RNGstream", "SPRNG"), 
 cleanup = TRUE, unload = FALSE, ...)
 {
-    balancing <- match.arg(balancing)
+## common stuff for snow and multicore
+    rng.type <- match.arg(rng.type) 
     ## if object name exists in global env, make a copy as tmp, and put back in the end
     if (!use.env && exists(name, envir=.GlobalEnv)) {
         assign("tmp", get(name, envir=.GlobalEnv))
@@ -17,56 +19,71 @@ cleanup = TRUE, unload = FALSE, ...)
             name <- ".DcloneEnv"
         assign(name, as.environment(cldata), envir=.GlobalEnv)
     }
-    ## loads lib on each worker
-    if (!is.null(lib)) {
-        pkglist <- unique(unlist(clusterEvalQ(cl, .packages())))
-        for (i in lib)
-            if (!(i %in% pkglist))
-                eval(parse(text=paste("clusterEvalQ(cl, library(", i, "))", sep="")))
-    }
-    ## set seed on each worker
-    rng.type <- match.arg(rng.type) 
-    if (rng.type != "none") {
-        clusterSetupRNG(cl, type = rng.type)
-    }
-    ## sets common working directory, but dir can be a vector as well
-    if (!is.null(dir)) {
-        if (cleanup)
-            dirold <- clusterEvalQ(cl, getwd())
-        dir <- rep(dir, length(cl))[1:length(cl)]
-        dirnew <- lapply(dir, function(z) paste("setwd(\"", z, "\")", sep=""))
-        parLapply(cl, dirnew, function(z) eval(parse(text=z)))
-    }
-    ## evaluates literal expressions if needed
-    if (!is.null(evalq)) {
-        for (i in evalq)
-            eval(parse(text=paste("clusterEvalQ(cl,", i, ")")))
-    }
     ## place object name into global env (clusterExport can reach it)
     if (!use.env)
         assign(name, cldata, envir = .GlobalEnv)
-    clusterExport(cl, name)
-    ## parallel work done here according to balancing
-    res <- switch(balancing,
-        "none" = parLapply(cl, seq, fun, ...),
-        "load" = clusterApplyLB(cl, seq, fun, ...),
-        "size" = parLapplySB(cl, seq, size=size, fun, ...),
-        "both" = parLapplySLB(cl, seq, size=size, fun, ...))
-    if (cleanup) {
-        ## remove cldata by name
-        eval(parse(text=paste("clusterEvalQ(cl, rm(", name, "))")))
-        ## set old dir
-        if (!is.null(dir)) {
-            dirold <- lapply(dirold, function(z) paste("setwd(\"", z, "\")", sep=""))
-            parLapply(cl, dirold, function(z) eval(parse(text=z)))
+## snow cluster
+    if (inherits(cl, "cluster")) {
+        require(snow)
+        balancing <- match.arg(balancing)
+        ## loads lib on each worker
+        if (!is.null(lib)) {
+            pkglist <- unique(unlist(clusterEvalQ(cl, .packages())))
+            for (i in lib)
+                if (!(i %in% pkglist))
+                    eval(parse(text=paste("clusterEvalQ(cl, library(", i, "))", sep="")))
         }
-    }
-    ## unload libs
-    if (unload && !is.null(lib)) {
-        for (i in lib)
-            if (!(i %in% pkglist))
-                eval(parse(text=paste("clusterEvalQ(cl, detach(package:", 
-                    i, ", unload=TRUE))", sep="")))
+        ## set seed on each worker
+        if (rng.type != "none") {
+            clusterSetupRNG(cl, type = rng.type)
+        }
+        ## sets common working directory, but dir can be a vector as well
+        if (!is.null(dir)) {
+            if (cleanup)
+                dirold <- clusterEvalQ(cl, getwd())
+            dir <- rep(dir, length(cl))[1:length(cl)]
+            dirnew <- lapply(dir, function(z) paste("setwd(\"", z, "\")", sep=""))
+            parLapply(cl, dirnew, function(z) eval(parse(text=z)))
+        }
+        ## evaluates literal expressions if needed
+        if (!is.null(evalq)) {
+            for (i in evalq)
+                eval(parse(text=paste("clusterEvalQ(cl,", i, ")")))
+        }
+        ## grab data list/env from global env
+        clusterExport(cl, name)
+        ## parallel work done here according to balancing
+        res <- switch(balancing,
+            "none" = parLapply(cl, seq, fun, ...),
+            "load" = clusterApplyLB(cl, seq, fun, ...),
+            "size" = parLapplySB(cl, seq, size=size, fun, ...),
+            "both" = parLapplySLB(cl, seq, size=size, fun, ...))
+        if (cleanup) {
+            ## remove cldata by name
+            eval(parse(text=paste("clusterEvalQ(cl, rm(", name, "))")))
+            ## set old dir
+            if (!is.null(dir)) {
+                dirold <- lapply(dirold, function(z) paste("setwd(\"", z, "\")", sep=""))
+                parLapply(cl, dirold, function(z) eval(parse(text=z)))
+            }
+        }
+        ## unload libs
+        if (unload && !is.null(lib)) {
+            for (i in lib)
+                if (!(i %in% pkglist))
+                    eval(parse(text=paste("clusterEvalQ(cl, detach(package:", 
+                        i, ", unload=TRUE))", sep="")))
+        }
+    } else {
+## multicore
+        require(parallel)
+        res <- mclapply(seq, fun, ...,
+            mc.set.seed = (rng.type == "none"),
+            mc.silent = as.logical(getOption("dcoptions")$verbose), 
+            mc.cores = cl,
+            mc.cleanup = cleanup, 
+            mc.preschedule = TRUE, 
+            mc.allow.recursive = TRUE)
     }
     res
 }
