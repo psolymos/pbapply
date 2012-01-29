14270f8b439a5bc1b25c0f027c0cfd66ad53148b
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-29 08:27:21 +0000

    line break rem on load

--- pkg/ResourceSelection/R/zzz.R
+++ pkg/ResourceSelection/R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname){
     ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), 
                     fields=c("Version", "Date"))
-    packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2], "\n"))
+    packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2]))
     invisible(NULL)
 }
 
--- pkg/detect/R/zzz.R
+++ pkg/detect/R/zzz.R
@@ -3,7 +3,7 @@
 .onAttach <- function(libname, pkgname){
     ver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), 
                     fields=c("Version", "Date"))
-    packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2], "\n"))
+    packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2]))
     invisible(NULL)
 }
 
