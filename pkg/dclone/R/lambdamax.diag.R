ddc5938340d011ba20145f27739c465763c97821
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-12 04:11:49 +0000

    diag now generic

--- pkg/dclone/R/lambdamax.diag.R
+++ pkg/dclone/R/lambdamax.diag.R
@@ -1,17 +1,5 @@
 lambdamax.diag <-
-function(x)
+function(x, ...)
 {
-#    npar <- nvar(x)
-#    y <- as.matrix(x) # mcmcapply(x, array)
-#    rval <- if (npar == 1) {
-#        sd(y)^2
-#    } else {
-#        max(eigen(var(y), symmetric=TRUE, only.values=TRUE)$val)
-#    }
-    rval <- max(eigen(var(as.matrix(x)), symmetric=TRUE, only.values=TRUE)$values)
-    class(rval) <- "lambdamax.diag"
-#    attr(rval, "npar") <- npar
-    attr(rval, "npar") <- nvar(x)
-    attr(rval, "n.clones") <- nclones(x)
-    rval
+    UseMethod("lambdamax.diag")
 }
