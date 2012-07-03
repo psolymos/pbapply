3f98c92880621a4fc1357b21f5dff765a2412360
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-03 18:00:55 +0000

    dciid can accept vectors

--- pkg/dclone/R/dclone.dciid.R
+++ pkg/dclone/R/dclone.dciid.R
@@ -1,12 +1,25 @@
 dclone.dciid <-
 function(x, n.clones = 1, attrib = TRUE, ...)
 {
-    rval <- dclone.default(x, n.clones=n.clones, attrib=attrib, ...)
     iid <- attr(x, "iid")
-    for (i in iid) {
-        val <- x[,i]
+    if (is.null(iid)) {
+        rval0 <- dclone.default(x, n.clones=n.clones, attrib=attrib, ...)
+        if (n.clones < 2)
+            return(rval0)
+        val <- x
         tmp <- t(sapply(val, "+", (1:n.clones*max(val)) - max(val)))
-        rval[,i] <- as.numeric(tmp)
+        rval <- as.numeric(tmp)
+        iid <- character(0)
+        attributes(rval) <- attributes(rval0)
+    } else {
+        rval <- dclone.default(x, n.clones=n.clones, attrib=attrib, ...)
+        if (n.clones < 2)
+            return(rval)
+        for (i in iid) {
+            val <- x[,i]
+            tmp <- t(sapply(val, "+", (1:n.clones*max(val)) - max(val)))
+            rval[,i] <- as.numeric(tmp)
+        }
     }
     attr(attr(rval, "n.clones"), "method") <- "iid"
     attr(attr(attr(rval, "n.clones"), "method"), "iid") <- iid
