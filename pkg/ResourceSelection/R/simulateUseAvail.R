566a35b1e04c4b9d9f4f1a624b38e69af91f28bd
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-01 19:36:47 +0000

    simulate RSPF

---
+++ pkg/ResourceSelection/R/simulateUseAvail.R
@@ -0,0 +1,14 @@
+simulateUsedAvail <- 
+function(data, parms, n.used, m, link="logit") 
+{
+    X <- model.matrix(~., data)
+    if (ncol(X) != length(parms))
+        stop("length of 'parms' incompatible with 'data'")
+    n.sites <- nrow(X)
+    n.avail <- n.used * m
+    linkinvfun <- binomial(link=make.link(link))$linkinv
+    p <- drop(linkinvfun(X %*% parms))
+    id1 <- sample.int(n.sites, n.used, replace = TRUE, prob = p)
+    id2 <- sample.int(n.sites, n.avail, replace = TRUE)
+    data.frame(status=c(rep(1, n.used), rep(0, n.avail)), data[c(id1, id2),])
+}
