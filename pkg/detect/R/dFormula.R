22d968c9074ed22cca4c9cb676fddbafc5187188
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-03 16:06:42 +0000

    new Formula interface

---
+++ pkg/detect/R/dFormula.R
@@ -0,0 +1,51 @@
+dFormula <- 
+function(formula, data, subset, drop=TRUE, ...)
+{
+    require(Formula)
+    mf <- match.call(expand.dots = FALSE)
+    m <- match(c("formula", "data", "subset"), names(mf), 0)
+    mf <- mf[c(1, m)]
+    f <- Formula(formula)
+    mf[[1]] <- as.name("model.frame")
+    mf$formula <- f
+    mf$na.action <- na.pass
+    mf <- eval(mf, parent.frame())
+    st <- length(f)
+    if (!(st[1] %in% 1:2))
+        stop("LHS ill-defined")
+    if (!(st[2] %in% 2:3))
+        stop("RHS ill-defined")
+    if (st[1] == 1) {
+        Y <- model.response(mf)
+        Y <- unname(Y)
+        Y <- drop(Y)
+        if (NCOL(Y) > 1)
+            stop("response matrix not allowed without methodology")
+        D <- NULL
+    } else {
+        if (st[1] >2)
+            stop(">2 parts in LHS not supported")
+        Y <- drop(data.matrix(model.part(f, data=mf, lhs=1)[[1]]))
+        D <- drop(data.matrix(model.part(f, data=mf, lhs=2)[[1]]))
+        if (NCOL(Y) != NCOL(D))
+            stop("ncol must be equal in response and methodology")
+    }
+    if (st[2] == 2) {
+        X <- model.matrix(f, data = mf, rhs = 1)
+        Z <- model.matrix(f, data = mf, rhs = 2)
+        Q <- NULL
+        st[2] <- 0
+    } else {
+        Q <- model.matrix(f, data = mf, rhs = 1)
+        X <- model.matrix(f, data = mf, rhs = 2)
+        Z <- model.matrix(f, data = mf, rhs = 3)
+        st[2] <- 1
+    }
+    checkDesign(Y, D, X, Z, Q)
+    if (!drop && is.null(dim(Y))) {
+        Y <- data.matrix(Y)
+        if (!is.null(D))
+            D <- data.matrix(D)
+    }
+    list(Y=Y,D=D,Q=Q,X=X,Z=Z,model=mf,formula=f)
+}
