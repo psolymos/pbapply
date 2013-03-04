31f64d6f243a365bda715454f988852ba41124d3
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-04 07:49:51 +0000

    length 1 chr model

--- pkg/dclone/R/custommodel.R
+++ pkg/dclone/R/custommodel.R
@@ -4,13 +4,15 @@ function (model, exclude = NULL, digits = 5)
     if (is.function(model)) {
         model.text <- c("model", 
             R2WinBUGS:::replaceScientificNotationR(body(model), 
-            digits=digits))
+                digits=digits))
 #        model.text <- attr(model, "source")
 #        model.text <- sub("^\\s*function\\s*\\(\\s*\\)", "model", 
 #            model.text)
         model.text <- gsub("%_%", "", model.text)
     } else {
         model.text <- as.character(unlist(model))
+        if (length(model.text) < 2)
+            model.text <- strsplit(model.text, "\n")[[1]]
     }
     incl <- 1:length(model.text)
     if (!is.null(exclude))
