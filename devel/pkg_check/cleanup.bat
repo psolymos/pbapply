872b0c4e16d7955dcc371228ff0ac3b1ac3f6ff1
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-10 18:06:32 +0000

    pkg testing facility extended

---
+++ devel/pkg_check/cleanup.bat
@@ -0,0 +1,1 @@
+rmdir c:\svn\dcr\devel\pkg_check\tests /s /q
--- devel/pkg_check/pkg_check.bat
+++ devel/pkg_check/pkg_check.bat
@@ -1,3 +1,5 @@
+:: cleanup
+::rmdir c:\svn\dcr\devel\pkg_check\tests /s /q
 :: export pkg dir
 svn export c:\svn\dcr\pkg\ c:\svn\dcr\devel\pkg_check\tests\
 svn export c:\svn\mefa\pkg\mefa c:\svn\dcr\devel\pkg_check\tests\mefa
@@ -7,22 +9,22 @@ R CMD BATCH --vanilla c:\svn\dcr\devel\pkg_check\updates.R c:\svn\dcr\devel\pkg_
 :: change dir to test
 cd c:\svn\dcr\devel\pkg_check\tests\
 :: export pkg dirs from svn
-R CMD build pbapply
 R CMD build dclone
 R CMD build dcmle
 R CMD build detect
-R CMD build PVAClone
-R CMD build sharx
-R CMD build ResourceSelection
 R CMD build mefa
 R CMD build mefa4
+R CMD build pbapply
+R CMD build PVAClone
+R CMD build ResourceSelection
+R CMD build sharx
 :: check pkgs
-R CMD check pbapply*.tar.gz --as-cran
-R CMD check dclone*.tar.gz --as-cran
-R CMD check dcmle*.tar.gz --as-cran
-R CMD check detect*.tar.gz --as-cran
-R CMD check PVAClone*.tar.gz --as-cran
-R CMD check sharx*.tar.gz --as-cran
-R CMD check ResourceSelection*.tar.gz --as-cran
-R CMD check mefa*.tar.gz --as-cran
-R CMD check mefa4*.tar.gz --as-cran
+R CMD check dclone_*.tar.gz --as-cran
+R CMD check dcmle_*.tar.gz --as-cran
+R CMD check detect_*.tar.gz --as-cran
+R CMD check mefa_*.tar.gz --as-cran
+R CMD check mefa4_*.tar.gz --as-cran
+R CMD check pbapply_*.tar.gz --as-cran
+R CMD check PVAClone_*.tar.gz --as-cran
+R CMD check ResourceSelection_*.tar.gz --as-cran
+R CMD check sharx_*.tar.gz --as-cran
