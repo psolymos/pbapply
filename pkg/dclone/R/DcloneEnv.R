d73a296543e3f1e3c1996646bb5ca95c464b0cc4
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-11 05:38:32 +0000

    functions for accessing .envs

---
+++ pkg/dclone/R/DcloneEnv.R
@@ -0,0 +1,44 @@
+## environments for parallel computing
+.DcloneEnvModel <- new.env(parent=emptyenv())
+.DcloneEnvResults <- new.env(parent=emptyenv())
+if(getRversion() >= "2.15.1")
+    utils::globalVariables(c(".DcloneEnvModel", ".DcloneEnvResults"))
+
+.pullDcloneEnv <- function (x, type = c("model", "results"))
+{
+    switch(match.arg(type),
+        "model" = get(x, envir = .DcloneEnvModel),
+        "results" = get(x, envir = .DcloneEnvResults))
+}
+
+.pushDcloneEnv <- function (x, value, type = c("model", "results"))
+{
+    switch(match.arg(type),
+        "model" = assign(x, value, envir = .DcloneEnvModel),
+        "results" = assign(x, value, envir = .DcloneEnvResults))
+}
+
+.clearDcloneEnv <- function(..., list = character(), 
+    type = c("model", "results"))
+{
+    switch(match.arg(type),
+        "model" = rm(..., list = list, envir = .DcloneEnvModel),
+        "results" = rm(..., list = list, envir = .DcloneEnvResults))
+}
+
+.listDcloneEnv <- function (type = c("model", "results"))
+{
+    switch(match.arg(type),
+        "model" = ls(envir = .DcloneEnvModel),
+        "results" = ls(envir = .DcloneEnvResults))
+    
+}
+
+.existsDcloneEnv <- function (x, type = c("model", "results"))
+{
+    switch(match.arg(type),
+        "model" = exists(x, envir = .DcloneEnvModel),
+        "results" = exists(x, envir = .DcloneEnvResults))
+    
+}
+
