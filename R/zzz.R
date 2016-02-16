b45d365247654ffcc7d46cab597be04d9c39c202
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-02-16 21:20:16 +0100

    New progress bar type

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
     if (is.null(getOption("pboptions")))
-        options("pboptions"=list(type="txt",
+        options("pboptions"=list(type="custom",
             char="+", txt.width=50, gui.width=300, style=3, initial=0,
             title="R progress bar", label=""))
     invisible(NULL)
