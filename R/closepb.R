4b41e0c357667ccfd755c19719ace5203f5b2c6d
Peter Solymos <psolymos@gmail.com>
2016-02-18 22:28:42 -0700

    connection must be closed for all types

--- R/closepb.R
+++ R/closepb.R
@@ -1,7 +1,8 @@
 closepb <-
 function(pb)
 {
-    if (is.null(pb) || getOption("pboptions")$type == "timer")
+    #if (is.null(pb) || getOption("pboptions")$type == "timer")
+    if (is.null(pb))
         invisible(NULL) else close(pb)
 }
 
