96e9aac055f80e586be65c8bbf97ac78b84d57f2
Peter Solymos <psolymos@gmail.com>
2016-02-18 21:58:56 -0700

    set args according to pboptions

--- R/windows/startpb.R
+++ R/windows/startpb.R
@@ -4,8 +4,12 @@ function(min=0, max=1)
     if (dopb()) {
         control <- getOption("pboptions")
         pb <- switch(control$type,
-            timer = timerProgressBar(min, max, control$initial),
-            txt = txtProgressBar(min, max, initial = control$initial,
+            timer = timerProgressBar(min = min, max = max,
+                initial = control$initial,
+                style = control$style, width = control$txt.width,
+                char = control$char),
+            txt = txtProgressBar(min = min, max = max,
+                initial = control$initial,
                 style = control$style, width = control$txt.width,
                 char = control$char),
             win = winProgressBar(min = min, max = max,
