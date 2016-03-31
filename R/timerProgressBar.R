8558af2f4ce19d0dd2dd0a047821f13193c596fc
Peter Solymos <psolymos@gmail.com>
2016-03-31 12:27:57 -0600

    remaining time

--- R/timerProgressBar.R
+++ R/timerProgressBar.R
@@ -23,7 +23,7 @@ width = NA, title, label, style = 1, file = "")
     if (is.na(width))
         width <- options("width")[[1]]
 
-    up <- function(value) {
+    up1 <- function(value) {
         if (!is.finite(value) || value < min || value > max)
             return()
         time <- proc.time()[["elapsed"]] - .start
@@ -52,12 +52,41 @@ width = NA, title, label, style = 1, file = "")
         cat(paste("\r", bar, text), file = file)
         flush.console()
     }
+    up2 <- function(value) {
+        if (!is.finite(value) || value < min || value > max)
+            return()
+        time <- proc.time()[["elapsed"]] - .start
+        .i <<- value
+        i <- .i - .min
+        n <- .max - .min
+        #time <- time / (i / n) - time
+        leftTime <- if (i == 0)
+            getTimeAsString(NULL) else getTimeAsString(time)
+        #minLetters <- nchar("%%%.%%% ~00h 00m 00s", "w") # 2 decimals too much
+        minLetters <- nchar("%%%% ~00h 00m 00s", "w")
+
+        ## 79-24=55 > 50
+        txtWidth <- max(width, width - minLetters - 4)
+
+        #text <- paste0(sprintf("%-2.2f%%", 100 * i / n), " ~", leftTime)
+        text <- paste0(sprintf("%-2.0f%%", 100 * i / n), " ~", leftTime)
+        if(nchar(text, "w") < minLetters)
+            text <- paste(text, paste(rep(" ", minLetters - nchar(text, "w")),
+                collapse = ""))
+        if(txtWidth < 0)
+            cat("\r ", text, file = file)
+        bb <- paste(rep(char, ceiling(txtWidth * i / n)), collapse = "")
+        empty <- paste(rep(" ", floor(txtWidth * (1 - i / n))), collapse = "")
+        bar <- paste("  |", bb, empty, "|", sep = "")
+        cat(paste("\r", bar, text), file = file)
+        flush.console()
+    }
     kill <- function() if (!.killed) {
         cat("\n", file = file)
         flush.console()
         .killed <<- TRUE
     }
-#    up <- switch(style, up1, up2)
+    up <- switch(style, up1, up2)
     up(initial)
     structure(list(getVal = getVal, up = up, kill = kill),
         class = c("timerProgressBar","txtProgressBar"))
