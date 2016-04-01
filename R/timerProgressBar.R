5311b7227aa21e475e861bea67bd18394e26c8d5
Peter Solymos <psolymos@gmail.com>
2016-04-01 16:26:18 -0600

    5th style

--- R/timerProgressBar.R
+++ R/timerProgressBar.R
@@ -7,9 +7,9 @@ width = NA, title, label, style = 1, file = "")
         stop("'file' must be \"\" or an open connection object")
     if (max <= min)
         stop("must have 'max' > 'min'")
-    if (!(style %in% 1:4))
+    if (!(style %in% 1:5))
         style <- 1
-    if (style %in% 1:2)
+    if (style %in% c(1, 2, 5))
         .counter <- force(1)
 
     .start <- proc.time()[["elapsed"]]
@@ -65,8 +65,9 @@ width = NA, title, label, style = 1, file = "")
         #time <- time / (i / n) - time
         if (i != 0)
             .counter <<- .counter + 1
-        leftTime <- if (i == 0)
-            getTimeAsString(NULL) else getTimeAsString(time)
+        #leftTime <- if (i == 0)
+        #    getTimeAsString(NULL) else getTimeAsString(time)
+        leftTime <- getTimeAsString(time)
         minLetters <- nchar("%%%% ~00h 00m 00s", "w")
 
         ## 79-24=55 > 50
@@ -122,8 +123,9 @@ width = NA, title, label, style = 1, file = "")
         i <- .i - .min
         n <- .max - .min
         #time <- time / (i / n) - time
-        leftTime <- if (i == 0)
-            getTimeAsString(NULL) else getTimeAsString(time)
+        #leftTime <- if (i == 0)
+        #    getTimeAsString(NULL) else getTimeAsString(time)
+        leftTime <- getTimeAsString(time)
         minLetters <- nchar("%%%% ~00h 00m 00s", "w")
 
         ## 79-24=55 > 50
@@ -141,12 +143,42 @@ width = NA, title, label, style = 1, file = "")
         cat(paste("\r", bar, text), file = file)
         flush.console()
     }
+    ## throbber with elapsed and remaining time
+    up5 <- function(value) {
+        if (!is.finite(value) || value < min || value > max)
+            return()
+        time0 <- proc.time()[["elapsed"]] - .start
+        .i <<- value
+        i <- .i - .min
+        n <- .max - .min
+        time <- time0 / (i / n) - time0
+        if (i != 0)
+            .counter <<- .counter + 1
+        spentTime <- paste0("  elapsed =", getTimeAsString(time0))
+        leftTime <- if (i == 0)
+            "" else paste0(", remaining ~", getTimeAsString(time))
+        minLetters <- nchar("%%%% ~00h 00m 00s", "w")
+
+        ## 79-24=55 > 50
+        txtWidth <- max(width, width - minLetters - 4)
+
+        text <- paste0(sprintf("%-2.0f%%", 100 * i / n), spentTime, leftTime)
+        if(nchar(text, "w") < minLetters)
+            text <- paste(text, paste(rep(" ", minLetters - nchar(text, "w")),
+                collapse = ""))
+        if(txtWidth < 0)
+            cat("\r ", text, file = file)
+        bb <- paste(rep(char, ceiling(txtWidth * i / n)), collapse = "")
+        bar <- c("|", "/", "-", "\\")[(.counter %% 4) + 1]
+        cat(paste("\r", bar, text), file = file)
+        flush.console()
+    }
     kill <- function() if (!.killed) {
         cat("\n", file = file)
         flush.console()
         .killed <<- TRUE
     }
-    up <- switch(style, up1, up2, up3, up4)
+    up <- switch(style, up1, up2, up3, up4, up5)
     up(initial)
     structure(list(getVal = getVal, up = up, kill = kill),
         class = c("timerProgressBar","txtProgressBar"))
