a224e8a357e5a50e292dc6b825abcea4342292e1
Peter Solymos <psolymos@gmail.com>
2016-09-08 12:50:18 -0600

    cl option worked out

--- R/pblapply.R
+++ R/pblapply.R
@@ -4,16 +4,62 @@ function (X, FUN, ..., cl = NULL)
     FUN <- match.fun(FUN)
     if (!is.vector(X) || is.object(X))
         X <- as.list(X)
-    B <- length(X)
-    if (!(interactive() && dopb() && B >= 1))
-        return(lapply(X, FUN, ...))
-    pb <- startpb(0, B)
-    rval <- vector("list", B)
-    for (i in 1:B) {
-        rval[i] <- list(FUN(X[[i]], ...))
-        setpb(pb, i)
+    ## catch single node requests and forking on Windows
+    if (!is.null(cl)) {
+        if (inherits(cl, "cluster") && length(cl) < 2L)
+            cl <- NULL
+        if (!inherits(cl, "cluster") && cl < 2)
+            cl <- NULL
+        if (!inherits(cl, "cluster") && .Platform$OS.type == "windows")
+            cl <- NULL
     }
-    closepb(pb)
+    ## sequential evaluation
+    if (is.null(cl)) {
+        B <- length(X)
+        if (!(interactive() && dopb() && B >= 1))
+            return(lapply(X, FUN, ...))
+        pb <- startpb(0, B)
+        on.exit(closepb(pb), add = TRUE)
+        rval <- vector("list", B)
+        for (i in seq_len(B)) {
+            rval[i] <- list(FUN(X[[i]], ...))
+            setpb(pb, i)
+        }
+    ## parallel evaluation
+    } else {
+        ## snow type cluster
+        if (inherits(cl, "cluster")) {
+            if (!(interactive() && dopb() && length(X) >= 1))
+                return(parallel::parLapply(cl, X, FUN, ...))
+            ## define split here and use that for counter
+            Split <- splitpb(length(X), length(cl))
+            B <- length(Split)
+            pb <- startpb(0, B)
+            on.exit(closepb(pb), add = TRUE)
+            rval <- vector("list", B)
+            for (i in seq_len(B)) {
+                rval[i] <- list(parallel::parLapply(cl, X[Split[[i]]], FUN, ...))
+                setpb(pb, i)
+            }
+        ## multicore type forking
+        } else {
+            if (!(interactive() && dopb() && length(X) >= 1))
+                return(parallel::mclapply(X, FUN, ..., mc.cores = as.integer(cl)))
+            ## define split here and use that for counter
+            Split <- splitpb(length(X), as.integer(cl))
+            B <- length(Split)
+            pb <- startpb(0, B)
+            on.exit(closepb(pb), add = TRUE)
+            rval <- vector("list", B)
+            for (i in seq_len(B)) {
+                rval[i] <- list(parallel::mclapply(X[Split[[i]]], FUN, ...,
+                    mc.cores = as.integer(cl)))
+                setpb(pb, i)
+            }
+        }
+        ## assemble output list
+        rval <- do.call(c, rval, quote = TRUE)
+    }
     names(rval) <- names(X)
     rval
 }
