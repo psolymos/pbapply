97255e9b4188eb89e4dad1b406bed95c318ff4dd
Peter Solymos <psolymos@gmail.com>
2016-09-08 19:45:56 -0600

    lazy math replaced by exact math

--- R/splitpb.R
+++ R/splitpb.R
@@ -4,8 +4,10 @@ function(nx, ncl, nout = NULL)
     i <- seq_len(nx)
     if (ncl == 0L)
         return(list())
+    #k <- if (is.null(nout))
+    #    1L else i[which.min(abs(ceiling(nx / (i*ncl)) - nout))]
     k <- if (is.null(nout))
-        1L else i[which.min(abs(ceiling(nx / (i*ncl)) - nout))]
+        1L else max(1L, ceiling(ceiling(nx / ncl) / nout))
     g <- 1L + (i - 1L) %/% as.integer(ncl * k)
     structure(split(i, g), names = NULL)
 }
