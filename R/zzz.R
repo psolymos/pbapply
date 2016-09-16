c0fcfb1a682aaa901d7ebc28bae328fa3541d1b5
Peter Solymos <psolymos@gmail.com>
2016-09-15 23:23:42 -0600

    wiser loading of options

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,19 @@
 .onLoad <- function(libname, pkgname){
-    if (is.null(getOption("pboptions"))) {
-        type <- if (interactive())
-            "timer" else "none"
-        options("pboptions" = list(
-            type = type,
-            char = "+",
-            txt.width = 50,
-            gui.width = 300,
-            style = 3,
-            initial = 0,
-            title = "R progress bar",
-            label = "",
-            nout = 100L))
-    }
+    opts <- list(
+        type = if (interactive()) "timer" else "none",
+        char = "+",
+        txt.width = 50,
+        gui.width = 300,
+        style = 3,
+        initial = 0,
+        title = "R progress bar",
+        label = "",
+        nout = 100L)
+    optsx <- getOption("pboptions")
+    if (!is.null(optsx))
+        for (i in intersect(names(opts), names(optsx)))
+            opts[[i]] <- optsx[[i]]
+    options("pboptions" = opts)
     invisible(NULL)
 }
 
