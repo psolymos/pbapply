1b167294b8425736bff96e3d1a407ca971a0dfaf
Peter Solymos <psolymos@gmail.com>
2016-10-14 10:38:45 -0600

    more testing: timings

--- tests/tests.R
+++ tests/tests.R
@@ -9,7 +9,17 @@ example(apply, run.dontrun = TRUE)
 example(splitpb, run.dontrun = TRUE)
 example(timerProgressBar, run.dontrun = TRUE)
 
-## --- simple timings ---
+## --- test for NULL case in lapply ---
+
+l <- list(a = 1, 2, c = -1)
+f <- function(z) if (z < 0) return(NULL) else return(2 * z)
+r1 <- lapply(l, f)
+r2 <- pblapply(l, f)
+r1
+r2
+stopifnot(identical(r1, r2))
+
+## --- timings ---
 
 if (FALSE) {
 
@@ -34,19 +44,29 @@ function(X, FUN, ...)
 }
 
 i <- seq_len(100)
-system.time(x1 <- lapply(i, function(i) Sys.sleep(0.1)))
-system.time(x1 <- lapply_pb(i, function(i) Sys.sleep(0.1)))
-#system.time(x1 <- l_ply(i, function(i) Sys.sleep(0.1), .progress="text"))
-system.time(x1 <- pblapply(i, function(i) Sys.sleep(0.1)))
+t1 <- system.time(lapply(i, function(i) Sys.sleep(0.1)))
+t2 <- system.time(lapply_pb(i, function(i) Sys.sleep(0.1)))
+#t3 <- system.time(l_ply(i, function(i) Sys.sleep(0.1), .progress="text"))
+t4 <- system.time(pblapply(i, function(i) Sys.sleep(0.1)))
+
+#f <- function(n, s=0.1) {
+#    i <- seq_len(n)
+#    t1 <- system.time(lapply(i, function(i) Sys.sleep(s)))
+#    t2 <- system.time(lapply_pb(i, function(i) Sys.sleep(s)))
+#    t3 <- system.time(l_ply(i, function(i) Sys.sleep(s), .progress="text"))
+#    t4 <- system.time(pblapply(i, function(i) Sys.sleep(s)))
+#    rbind(cbind(t1, t2, t3, t4), expected=n*s)
+#}
+
+#n <- c(10, 50, 100, 200, 500)
+#x <- lapply(n, f)
+#m <- t(sapply(x, function(z) z["elapsed",] - z["expected",]))
+
+#op <- par(mfrow=c(1,2))
+#matplot(n, m, type="b", lty=1, ylab="Overhead (sec)", xlab="# iterations")
+#legend("topleft", bty="n", col=1:4, pch=as.character(1:4), text.col=1:4,
+#    legend=c("lapply","lapply_pb","l_ply","pbapply"))
+#matplot(n, m/n, type="b", lty=1, ylab="Overhead / # iterations (sec)", xlab="# iterations")
+#par(op)
 
 }
-
-## --- test for NULL case in lapply ---
-
-l <- list(a = 1, 2, c = -1)
-f <- function(z) if (z < 0) return(NULL) else return(2 * z)
-r1 <- lapply(l, f)
-r2 <- pblapply(l, f)
-r1
-r2
-stopifnot(identical(r1, r2))
