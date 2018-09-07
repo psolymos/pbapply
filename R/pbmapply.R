ee9f160c429e84234f64c63781dd94ba23537610
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-09-07 17:21:30 +0200

    pbmapply

---
+++ R/pbmapply.R
@@ -0,0 +1,19 @@
+.pb_env <- new.env()
+
+pbmapply <- function(FUN, ..., MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = TRUE) {
+
+    .pb_env$FUN <- FUN
+    .pb_env$MAX <- max(sapply(list(...), length))
+    .pb_env$VALUE <- 0
+    .pb_env$pb <- pbapply::startpb(0, .pb_env$MAX)
+    on.exit(closepb(.pb_env$pb), add=TRUE)
+    on.exit(rm(list=ls(envir=.pb_env), envir=.pb_env), add=TRUE)
+
+    suppressMessages(trace(quote(FUN), exit = quote({
+        .pb_env$VALUE <- .pb_env$VALUE + 1
+        pbapply::setpb(.pb_env$pb, .pb_env$VALUE)
+    }), where = .pb_env, print = FALSE))
+
+    result <- mapply(.pb_env$FUN, ..., MoreArgs = MoreArgs, SIMPLIFY = SIMPLIFY, USE.NAMES = USE.NAMES)
+    result
+}
