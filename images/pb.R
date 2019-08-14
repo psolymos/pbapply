40f926701c955538b8b7030358938042f45a9b7f
Peter Solymos <psolymos@gmail.com>
2019-08-14 15:34:00 -0600

    including code for download and revdep stats

--- images/pb.R
+++ images/pb.R
@@ -1,4 +1,4 @@
-## code for terminal gif using licecap (http://www.cockos.com/licecap/)
+## >>> code for terminal gif using licecap (http://www.cockos.com/licecap/)
 
 library(pbapply)
 
@@ -23,4 +23,16 @@ tmp <- pblapply(seq_len(B), fun)
 
 ## restore default settings
 pboptions(pbo)
+
+## >>> dowload and revdep stats to save periodically
+
+library("ggplot2")
+library("dlstats")
+pkg <- "pbapply"
+x <- cran_stats(pkg)
+rd <- devtools::revdep(pkg)
+p <- ggplot(x[x$start < max(x$start),], aes(end, downloads)) +
+    geom_line() + geom_smooth() +
+    labs(title=paste0(pkg, " (", length(rd), " revdeps)"))
+ggsave(paste0("images/pbapply-downloads-", substr(Sys.Date(), 1, 7), ".png"), p)
 
