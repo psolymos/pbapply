a22d90226ace473112b7dbdda321e3f0487df39b
Peter Solymos <psolymos@gmail.com>
2019-08-30 20:35:10 -0600

    downloads

--- images/pb.R
+++ images/pb.R
@@ -29,10 +29,41 @@ pboptions(pbo)
 library("ggplot2")
 library("dlstats")
 pkg <- "pbapply"
+dt <- as.POSIXlt(Sys.Date())
+yyyymm <- paste0(dt$year+1900, "-",
+    if (dt$mon < 10) "0" else "", dt$mon) # mon is 0-11
 x <- cran_stats(pkg)
 rd <- devtools::revdep(pkg)
 p <- ggplot(x[x$start < max(x$start),], aes(end, downloads)) +
     geom_line() + geom_smooth() +
     labs(title=paste0(pkg, " (", length(rd), " revdeps)"))
-ggsave(paste0("images/pbapply-downloads-", substr(Sys.Date(), 1, 7), ".png"), p)
+ggsave(paste0("images/pbapply-downloads-", yyyymm, ".png"), p)
+
+## >>> prepare CRAN submission
+
+submit_cran_template <- function(path = ".") {
+    news <- readLines(file.path(path, "NEWS.md"))
+    i <- which(startsWith(news, "##"))[1L:2L]
+    latest <- news[i[1L]:(i[2L]-1)]
+    latest <- latest[-1]
+    latest <- latest[latest != ""]
+    descr <- read.dcf(file.path(path, "DESCRIPTION"))
+    ver <- unname(descr[1L,"Version"])
+    pkg <- unname(descr[1L,"Package"])
+    maint <- unname(descr[1L,"Maintainer"])
+    out <- c(
+        "Dear CRAN Maintainers,\n\n",
+        "This is an update (version ", ver, ") of the ",
+        pkg, " R extension package.\n\n",
+        "The package includes the following changes:\n\n",
+        paste(latest, collapse="\n"),
+        "\n\nThe package tarball passed R CMD check --as-cran ",
+        "without error/warning/note on Mac (current R), ",
+        "Linux (old, current, devel), and Windows (current, devel R).\n\n",
+        "Best wishes,\n\n",
+        maint, "\npackage maintainer")
+    out
+}
+
+cat(submit_cran_template(), sep="")
 
