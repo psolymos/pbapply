5f1f3219d9595a808e963782db4aed6c6e549535
Peter Solymos <psolymos@gmail.com>
2022-04-24 16:30:20 -0600

    Add .mapply draft re #46

--- R/pbmapply.R
+++ R/pbmapply.R
@@ -23,3 +23,26 @@ function(FUN, ..., MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = TRUE)
     mapply(.pb_env$FUN, ..., MoreArgs = MoreArgs,
         SIMPLIFY = SIMPLIFY, USE.NAMES = USE.NAMES)
 }
+
+pb.mapply <-
+function(FUN, dots, MoreArgs)
+{
+    ## setting up counter in .pb_env
+    .pb_env$FUN <- FUN
+    .pb_env$MAX <- max(sapply(dots, length))
+    .pb_env$VALUE <- 0
+    .pb_env$pb <- startpb(0, .pb_env$MAX)
+    on.exit(closepb(.pb_env$pb), add=TRUE)
+    on.exit(rm(list=ls(envir=.pb_env), envir=.pb_env), add=TRUE)
+    ## setting tracer
+    suppressWarnings(suppressMessages(trace(quote(FUN),
+        exit = quote({
+            .pb_env <- pbapply::.pb_env
+            .pb_env$VALUE <- .pb_env$VALUE + 1
+            pbapply::setpb(.pb_env$pb, .pb_env$VALUE)
+        }),
+        where = .pb_env,
+        print = FALSE)))
+    ## piggy back on .mapply
+    .mapply(.pb_env$FUN, dots = dots, MoreArgs = MoreArgs)
+}
